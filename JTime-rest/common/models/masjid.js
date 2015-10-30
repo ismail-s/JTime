688c08dc5cdac0c79961aa10c5476e58522c95ba
Ismail <astroman3d@gmail.com>
2015-10-30 14:45:15 +0000

    Add validation to name and type fields, fix #4

--- JTime-rest/common/models/masjid.js
+++ JTime-rest/common/models/masjid.js
@@ -2,4 +2,5 @@ var updateTimestamp = require("../updateTimestamp");
 
 module.exports = function(Masjid) {
     Masjid.observe('before save', updateTimestamp);
+    Masjid.validatesLengthOf('name', {max: 50});
 };
--- JTime-rest/common/models/salaahtime.js
+++ JTime-rest/common/models/salaahtime.js
@@ -2,4 +2,10 @@ var updateTimestamp = require("../updateTimestamp");
 
 module.exports = function(salaahTime) {
     salaahTime.observe('before save', updateTimestamp);
+    salaahTime.validatesInclusionOf('type', { in : 'fzamej'.split(''),
+            message: 'is not an allowed salaah type.'
+    });
+    salaahTime.validatesLengthOf('type', {
+        is: 1
+    });
 };
