e05dbbb86f6307b2bab860a2dafaf9f5fa9333bb
Ismail S <ismail-s.no-reply@github.com>
2015-11-25 16:04:11 +0000

    Add test for starting new activity

--- JTime-android/src/test/java/com/ismail_s/jtime/android/activity/AllMasjidsActivityTest.java
+++ JTime-android/src/test/java/com/ismail_s/jtime/android/activity/AllMasjidsActivityTest.java
@@ -1,15 +1,18 @@
 package com.ismail_s.jtime.android.activity;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.ListView;
 
 import com.ismail_s.jtime.android.BuildConfig;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowToast;
 
@@ -19,19 +22,40 @@ import static org.junit.Assert.assertTrue;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class AllMasjidsActivityTest {
+    ListActivity activity;
+    String[] masjids = new String[]{"one", "two", "three", "four", "five", "six"};
 
+    @Before
+    public void setupActivity() {
+        activity = Robolectric.setupActivity(AllMasjidsActivity.class);
+    }
+
+    public void clickOnItem(int position) {
+        ListView listView = activity.getListView();
+        View item = listView.getAdapter().getView(position, null, listView);
+        activity.getListView().performItemClick(item, position, item.getId());
+    }
     @Test
     public void testListActivityDisplaysToastsOnClickingListItems() throws Exception {
-        String[] masjids = new String[]{"one", "two", "three", "four", "five", "six"};
-        ListActivity activity = Robolectric.setupActivity(AllMasjidsActivity.class);
         assertTrue(activity != null);
-        ListView listView = activity.getListView();
         assertEquals(0, ShadowToast.shownToastCount());
         for (int i = 0; i <= 5; i++) {
-            View item = listView.getAdapter().getView(i, null, listView);
-            activity.getListView().performItemClick(item, i, item.getId());
+            clickOnItem(i);
+            // Robolectric won't open a new activity, so despite the call being made to start
+            // a new activity, we can ignore it for this test.
             assertEquals(i+1, ShadowToast.shownToastCount());
             assertEquals(masjids[i], ShadowToast.getTextOfLatestToast());
+        }
+    }
+
+    @Test
+    public void testListActivityStartsMasjidActivityWithCorrectIntent() {
+        for (int i = 0; i <= 5; i++) {
+            clickOnItem(i);
+            Intent expectedIntent = new Intent(activity, MasjidActivity.class);
+            expectedIntent.putExtra(Constants.MASJID_NAME, masjids[i]);
+            Intent actualIntent = Shadows.shadowOf(activity).getNextStartedActivity();
+            assertEquals(expectedIntent, actualIntent);
         }
     }
 }
