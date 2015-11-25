0355a4e82ff255aedba7c1fde081e07f4df8f160
Ismail S <ismail-s.no-reply@github.com>
2015-11-25 15:22:23 +0000

    Update test

--- JTime-android/src/test/java/com/ismail_s/jtime/android/activity/AllMasjidsActivityTest.java
+++ JTime-android/src/test/java/com/ismail_s/jtime/android/activity/AllMasjidsActivityTest.java
@@ -1,5 +1,9 @@
 package com.ismail_s.jtime.android.activity;
 
+import android.app.ListActivity;
+import android.view.View;
+import android.widget.ListView;
+
 import com.ismail_s.jtime.android.BuildConfig;
 
 import org.junit.Test;
@@ -7,7 +11,9 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowToast;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricGradleTestRunner.class)
@@ -16,6 +22,16 @@ public class AllMasjidsActivityTest {
 
     @Test
     public void testSomething() throws Exception {
-        assertTrue(Robolectric.setupActivity(AllMasjidsActivity.class) != null);
+        String[] masjids = new String[]{"one", "two", "three", "four", "five", "six"};
+        ListActivity activity = Robolectric.setupActivity(AllMasjidsActivity.class);
+        assertTrue(activity != null);
+        ListView listView = activity.getListView();
+        assertEquals(0, ShadowToast.shownToastCount());
+        for (int i = 0; i <= 5; i++) {
+            View item = listView.getAdapter().getView(i, null, listView);
+            activity.getListView().performItemClick(item, i, item.getId());
+            assertEquals(i+1, ShadowToast.shownToastCount());
+            assertEquals(masjids[i], ShadowToast.getTextOfLatestToast());
+        }
     }
 }
