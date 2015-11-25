b1f6ad13d2c8a583f5adb279f56f829906c3f721
Ismail S <ismail-s.no-reply@github.com>
2015-11-25 15:21:11 +0000

    Add a list of fake masjids

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/AllMasjidsActivity.java
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/AllMasjidsActivity.java
@@ -1,15 +1,32 @@
 package com.ismail_s.jtime.android.activity;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ismail_s.jtime.android.R;
 
-public class AllMasjidsActivity extends Activity {
+public class AllMasjidsActivity extends ListActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.all_masjids_activity);
+      super.onCreate(savedInstanceState);
+      String[] masjids = new String[]{"one", "two", "three", "four", "five", "six"};
+      setListAdapter(new ArrayAdapter<>(this, R.layout.list_item, masjids));
+      ListView listView = getListView();
+      listView.setTextFilterEnabled(true);
+      listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+              CharSequence text = ((TextView)view).getText();
+              Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+              toast.show();
+          }
+      });
   }
 }
