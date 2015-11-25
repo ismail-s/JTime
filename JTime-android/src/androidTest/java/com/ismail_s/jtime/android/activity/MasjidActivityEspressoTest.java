b6cdca433490fad431ee534cf3e1a11d3e36bc43
Ismail S <ismail-s.no-reply@github.com>
2015-11-25 16:25:22 +0000

    Rename file

--- JTime-android/src/androidTest/java/com/ismail_s/jtime/android/activity/MasjidActivityEspressoTest.java
+++ JTime-android/src/androidTest/java/com/ismail_s/jtime/android/activity/MasjidActivityEspressoTest.java
@@ -15,7 +15,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
-public class MainActivityEspressoTest {
+public class MasjidActivityEspressoTest {
 
 
     @Rule
