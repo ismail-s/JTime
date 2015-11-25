7f12fda08601f367579dc694c88cdccde12c1623
Ismail S <ismail-s.no-reply@github.com>
2015-11-25 14:06:49 +0000

    Rename class and fix imports

--- JTime-android/src/androidTest/java/com/ismail_s/jtime/android/activity/MainActivityEspressoTest.java
+++ JTime-android/src/androidTest/java/com/ismail_s/jtime/android/activity/MainActivityEspressoTest.java
@@ -1,10 +1,10 @@
-package com.android.activity;
+package com.ismail_s.jtime.android.activity;
 
-import com.android.R;
-import com.ismail_s.activity.DeckardActivity;
 
 import android.support.test.rule.ActivityTestRule;
 import android.test.suitebuilder.annotation.LargeTest;
+
+import com.ismail_s.jtime.android.R;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,12 +15,12 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
-public class DeckardEspressoTest  {
+public class MainActivityEspressoTest {
 
 
     @Rule
-    public ActivityTestRule<DeckardActivity> mActivityRule =
-            new ActivityTestRule<>(DeckardActivity.class);
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void testActivityShouldHaveText() throws InterruptedException {
