88357892c08490866de45fa52e5c7176366e3610
Ismail S <ismail-s.no-reply@github.com>
2015-12-08 21:50:28 +0000

    Add mock times to masjidActivity, create dummy restClient

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/MasjidActivity.java
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/MasjidActivity.java
@@ -2,11 +2,19 @@ package com.ismail_s.jtime.android.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.widget.TextView;
 
+import com.ismail_s.jtime.android.MasjidPojo;
 import com.ismail_s.jtime.android.R;
+import com.ismail_s.jtime.android.RestClient;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class MasjidActivity extends Activity {
+    private ArrayList<TextView> dateTextViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,20 @@ public class MasjidActivity extends Activity {
         String masjidName = getIntent().getStringExtra(Constants.MASJID_NAME);
         TextView masjidNameView = (TextView) findViewById(R.id.masjid_name);
         masjidNameView.setText(masjidName);
+        MasjidPojo masjidTimes = (new RestClient()).getMasjidTimes(masjidName);
+        dateTextViews.add((TextView) findViewById(R.id.fajr_date));
+        dateTextViews.add((TextView) findViewById(R.id.zohar_date));
+        dateTextViews.add((TextView) findViewById(R.id.asr_date));
+        dateTextViews.add((TextView) findViewById(R.id.magrib_date));
+        dateTextViews.add((TextView) findViewById(R.id.esha_date));
+        GregorianCalendar[] times = masjidTimes.getTimes();
+        for (int i = 0; i < dateTextViews.size(); i++) {
+            dateTextViews.get(i).setText(formatCalendar(times[i]));
+        }
+    }
+
+    private String formatCalendar(GregorianCalendar calendar) {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+        return formatter.format(calendar.getTime());
     }
 }
