d0d575ed00471937c67234150553c483feefd00d
Ismail S <ismail-s.no-reply@github.com>
2015-12-14 12:44:18 +0000

    Copy from MasjidActivity to NewMasjidFragment

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/NewMasjidFragment.java
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/NewMasjidFragment.java
@@ -7,12 +7,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.ismail_s.jtime.android.MasjidPojo;
 import com.ismail_s.jtime.android.R;
+import com.ismail_s.jtime.android.RestClient;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class NewMasjidFragment extends Fragment {
+    private ArrayList<TextView> dateTextViews = new ArrayList<>();
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -40,6 +48,28 @@ public class NewMasjidFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_new_masjid, container, false);
         TextView textView = (TextView) rootView.findViewById(R.id.section_label);
         textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        String masjidName = getActivity().getIntent().getStringExtra(Constants.MASJID_NAME);
+        TextView masjidNameView = (TextView) findViewById(R.id.masjid_name);
+        masjidNameView.setText(masjidName);
+        MasjidPojo masjidTimes = (new RestClient()).getMasjidTimes(masjidName);
+        dateTextViews.add((TextView) findViewById(R.id.fajr_date));
+        dateTextViews.add((TextView) findViewById(R.id.zohar_date));
+        dateTextViews.add((TextView) findViewById(R.id.asr_date));
+        dateTextViews.add((TextView) findViewById(R.id.magrib_date));
+        dateTextViews.add((TextView) findViewById(R.id.esha_date));
+        GregorianCalendar[] times = masjidTimes.getTimes();
+        for (int i = 0; i < dateTextViews.size(); i++) {
+            dateTextViews.get(i).setText(formatCalendar(times[i]));
+        }
         return rootView;
+    }
+
+    private String formatCalendar(GregorianCalendar calendar) {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        return formatter.format(calendar.getTime());
+    }
+
+    public View findViewById(int id) {
+        return getActivity().findViewById(id);
     }
 }
