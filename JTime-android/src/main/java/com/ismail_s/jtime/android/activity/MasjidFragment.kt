74ff2e2856a5cdeb34a75e3f9a83598e4e2e67c2
Ismail <ismail-s.no-reply@github.com>
2016-03-23 12:35:33 +0000

    Remove unnecessary import

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/MasjidFragment.kt
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/MasjidFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import com.ismail_s.jtime.android.R
 import com.ismail_s.jtime.android.RestClient
 import java.text.SimpleDateFormat
