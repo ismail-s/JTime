283bd5cbfb396995085a5780ad25c5f25c744472
Ismail S <ismail-s.no-reply@github.com>
2016-04-25 11:49:00 +0100

    Display map, handle long clicks on map

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/AddMasjidFragment.kt
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/AddMasjidFragment.kt
@@ -5,13 +5,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.ismail_s.jtime.android.R
 
-class AddMasjidFragment : Fragment() {
+class AddMasjidFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+    private var current_marker: Marker? = null
+    private var googleMap: GoogleMap? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val rootView = inflater!!.inflate(R.layout.fragment_add_masjid, container, false)
+        val mapFragment = MapFragment.newInstance()
+        mapFragment.getMapAsync(this)
+        childFragmentManager.beginTransaction()
+            .add(R.id.map_container, mapFragment).commit()
         return rootView
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        //TODO-add padding to take into account submit button
+        googleMap.setOnMapLongClickListener(this)
+        this.googleMap = googleMap
+    }
+
+    override fun onMapLongClick(point: LatLng) {
+        current_marker?.remove()
+        val marker_options = MarkerOptions().position(point).title("Masjid location")
+        current_marker = googleMap?.addMarker(marker_options)
     }
 }
