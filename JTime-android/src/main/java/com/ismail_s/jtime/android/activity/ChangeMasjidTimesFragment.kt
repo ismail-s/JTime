e6f93ab2f6c89afce77945f220da114742d816ba
Ismail <astroman3d@gmail.com>
2016-04-28 14:54:14 +0000

    Handle remaining button clicks, refactor code

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/ChangeMasjidTimesFragment.kt
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/ChangeMasjidTimesFragment.kt
@@ -88,23 +88,17 @@ class ChangeMasjidTimesFragment : Fragment(), View.OnClickListener {
                     masjidTimeTextbox.setText(t)
                 }
             }
-            R.id.up_button -> changeTimeAndSwitchToAnotherDay(dayOffset = -1)
-            R.id.down_button -> changeTimeAndSwitchToAnotherDay(dayOffset = 1)
-            R.id.left_button -> {
-                //Save time, switch to prev. salaah
-                //If invalid time, return straightaway
-            }
-            R.id.right_button -> {
-                //Save time, switch to next salaah
-                //If invalid time, return straightaway
-            }
-            R.id.copy_up_button -> changeTimeAndSwitchToAnotherDayAndCopyTime(-1)
-            R.id.copy_down_button -> changeTimeAndSwitchToAnotherDayAndCopyTime(1)
+            R.id.up_button -> saveTimeAndSwitchToAnotherDay(dayOffset = -1)
+            R.id.down_button -> saveTimeAndSwitchToAnotherDay(dayOffset = 1)
+            R.id.left_button -> saveTimeAndSwitchToPrevSalaah()
+            R.id.right_button -> saveTimeAndSwitchToNextSalaah()
+            R.id.copy_up_button -> saveTimeAndSwitchToAnotherDayAndCopyTime(-1)
+            R.id.copy_down_button -> saveTimeAndSwitchToAnotherDayAndCopyTime(1)
         }
     }
 
-    private fun changeTimeAndSwitchToAnotherDay(dayOffset: Int) {
-        changeTimeAndSwitchToAnotherDayAndThen(dayOffset) {
+    private fun saveTimeAndSwitchToAnotherDay(dayOffset: Int) {
+        saveTimeAndSwitchToAnotherDayAndThen(dayOffset) {
             previousDisplayedTime -> {
                 val times = currentMasjidPojo as MasjidPojo
                 var timeToDisplay: GregorianCalendar? = null
@@ -125,8 +119,8 @@ class ChangeMasjidTimesFragment : Fragment(), View.OnClickListener {
         }
     }
 
-    private fun changeTimeAndSwitchToAnotherDayAndCopyTime(dayOffset: Int) {
-        changeTimeAndSwitchToAnotherDayAndThen(dayOffset) {
+    private fun saveTimeAndSwitchToAnotherDayAndCopyTime(dayOffset: Int) {
+        saveTimeAndSwitchToAnotherDayAndThen(dayOffset) {
             previousDisplayedTime -> {
                 val formattedTime = formatCalendarAsTime(previousDisplayedTime)
                 masjidTimeTextbox.setText(formattedTime)
@@ -139,7 +133,100 @@ class ChangeMasjidTimesFragment : Fragment(), View.OnClickListener {
      * switch to a new day as per the dayOffset given. Then, run the "then"
      * parameter function which should sort out what the textbox now displays.
      */
-    private fun changeTimeAndSwitchToAnotherDayAndThen(dayOffset: Int, then: (previousDisplayedTime: GregorianCalendar) -> Unit) {
+    private fun saveTimeAndSwitchToAnotherDayAndThen(dayOffset: Int, then: (previousDisplayedTime: GregorianCalendar) -> Unit) {
+        saveTimeAndThen { newDate ->
+            //Switch to next day
+            val nextDate = date.clone() as GregorianCalendar
+            nextDate.add(Calendar.DAY_OF_MONTH, dayOffset)
+            val cb2 = object : RestClient.MasjidTimesCallback {
+                override fun onSuccess(times: MasjidPojo) {
+                    currentMasjidPojo = times
+                    date = nextDate
+                    then(newDate)
+                }
+
+                override fun onError(t: Throwable) {
+                    val s = "Failed to get times: " + t.message
+                    showShortToast(s)
+                    //TODO-should this (next) line be here
+                    (activity as MainActivity).switchToMasjidsFragment(masjidId, masjidName)
+                }
+            }
+            RestClient(activity).getMasjidTimes(masjidId, cb2, nextDate)
+        }
+    }
+
+    private fun saveTimeAndSwitchToNextSalaah() {
+        saveTimeAndThen { newDate ->
+            //Switch to next salaah
+            var newTime: GregorianCalendar? = null
+            when (currentSalaahType) {
+                SalaahType.FAJR -> {
+                    currentSalaahType = SalaahType.ZOHAR
+                    newTime = currentMasjidPojo.zoharTime
+                }
+                SalaahType.ZOHAR -> {
+                    currentSalaahType = SalaahType.ASR
+                    newTime = currentMasjidPojo.asrTime
+                }
+                SalaahType.ASR -> {
+                    currentSalaahType = SalaahType.MAGRIB
+                    newTime = currentMasjidPojo.magribTime
+                }
+                SalaahType.MAGRIB -> {
+                    currentSalaahType = SalaahType.ESHA
+                    newTime = currentMasjidPojo.eshaTime
+                }
+                SalaahType.ESHA -> {
+                    currentSalaahType = SalaahType.FAJR
+                    newTime = currentMasjidPojo.fajrTime
+                }
+            }
+            if (newTime == null) {
+                masjidTimeTextbox.setText("")
+            } else {
+                val formattedTime = formatCalendarAsTime(newTime)
+                masjidTimeTextbox.setText(formattedTime)
+            }
+        }
+    }
+
+    private fun saveTimeAndSwitchToPrevSalaah() {
+        saveTimeAndThen { newDate ->
+            //Switch to prev. salaah
+            var newTime: GregorianCalendar? = null
+            when (currentSalaahType) {
+                SalaahType.FAJR -> {
+                    currentSalaahType = SalaahType.ESHA
+                    newTime = currentMasjidPojo.eshaTime
+                }
+                SalaahType.ZOHAR -> {
+                    currentSalaahType = SalaahType.FAJR
+                    newTime = currentMasjidPojo.fajrTime
+                }
+                SalaahType.ASR -> {
+                    currentSalaahType = SalaahType.ZOHAR
+                    newTime = currentMasjidPojo.zoharTime
+                }
+                SalaahType.MAGRIB -> {
+                    currentSalaahType = SalaahType.ASR
+                    newTime = currentMasjidPojo.asrTime
+                }
+                SalaahType.ESHA -> {
+                    currentSalaahType = SalaahType.MAGRIB
+                    newTime = currentMasjidPojo.magribTime
+                }
+            }
+            if (newTime == null) {
+                masjidTimeTextbox.setText("")
+            } else {
+                val formattedTime = formatCalendarAsTime(newTime)
+                masjidTimeTextbox.setText(formattedTime)
+            }
+        }
+    }
+
+    private fun saveTimeAndThen(then: (newDate: GregorianCalendar) -> Unit) {
         //If invalid time, return straightaway
         val time = getTextboxTimeIfValid()
         if (time == null) {
@@ -162,24 +249,7 @@ class ChangeMasjidTimesFragment : Fragment(), View.OnClickListener {
             }
         }
         RestClient(activity).createOrUpdateMasjidTime(masjidId, currentSalaahType, newDate, cb1)
-        //Switch to next day
-        val nextDate = date.clone() as GregorianCalendar
-        nextDate.add(Calendar.DAY_OF_MONTH, dayOffset)
-        val cb2 = object : RestClient.MasjidTimesCallback {
-            override fun onSuccess(times: MasjidPojo) {
-                currentMasjidPojo = times
-                date = nextDate
-                then(newDate)
-            }
-
-            override fun onError(t: Throwable) {
-                val s = "Failed to get times: " + t.message
-                showShortToast(s)
-                //TODO-should this (next) line be here
-                (activity as MainActivity).switchToMasjidsFragment(masjidId, masjidName)
-            }
-        }
-        RestClient(activity).getMasjidTimes(masjidId, cb2, nextDate)
+        then(newDate)
     }
 
     /**
