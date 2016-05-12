807fadb85ab024997e5c9342ceeadb36410d00f9
Ismail S <ismail-s.no-reply@github.com>
2016-05-12 16:09:35 +0100

    Switch to support versions for fragments

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/MasjidsFragment.kt
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/MasjidsFragment.kt
@@ -1,10 +1,10 @@
 package com.ismail_s.jtime.android.activity
 
 
-import android.app.Fragment
-import android.app.FragmentManager
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.os.Bundle
-import android.support.v13.app.FragmentStatePagerAdapter
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
