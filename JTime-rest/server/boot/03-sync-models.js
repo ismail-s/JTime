6b25e081b0d519f1898b19c0e419f738850572d0
Ismail S <ismail-s.no-reply@github.com>
2016-06-08 13:52:09 +0100

    Get rid of warning about possible memory leak

--- JTime-rest/server/boot/03-sync-models.js
+++ JTime-rest/server/boot/03-sync-models.js
@@ -14,6 +14,11 @@ module.exports = function(app) {
     var builtinTables = ['user_table', 'accessToken', 'ACL', 'RoleMapping', 'Role'];
     var tables = customTables.concat(builtinTables);
     var dataSource = app.dataSources.postgres;
+    //This line prevents too many connections to the db being created.
+    //See https://github.com/strongloop/loopback-datasource-juggler/issues/805
+    //for more info.
+    dataSource.setMaxListeners(0);
+
     tables.forEach(function(table) {
         updateModelIfNecessary(table, dataSource);
     });
