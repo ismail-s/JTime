45447c49ae9a58d6406b532e23783c790a54728c
Ismail S <ismail-s.no-reply@github.com>
2016-06-09 17:19:08 +0100

    Make links clickable

--- JTime-android/src/main/java/com/ismail_s/jtime/android/activity/HelpFragment.kt
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/activity/HelpFragment.kt
@@ -1,16 +1,22 @@
 package com.ismail_s.jtime.android.activity
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.ismail_s.jtime.android.R
 
 class HelpFragment : BaseFragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         Log.d("jtime", "In onCreateView of HelpFragment with inflater $inflater & container $container")
-        return inflater!!.inflate(R.layout.fragment_help, container, false)
+        val rootView = inflater!!.inflate(R.layout.fragment_help, container, false)
+        val helpText = rootView.findViewById(R.id.label_help) as TextView
+        //Make the links in the help text clickable
+        helpText.setMovementMethod(LinkMovementMethod.getInstance())
+        return rootView
     }
 }
