0112411bde59c26526b77a7b4e8e89ffcd44711b
Ismail S <ismail-s.no-reply@github.com>
2016-06-14 15:31:22 +0100

    Create SalaahTimePojo

---
+++ JTime-android/src/main/java/com/ismail_s/jtime/android/SalaahTimePojo.kt
@@ -0,0 +1,13 @@
+package com.ismail_s.jtime.android
+
+import java.util.*
+
+class SalaahTimePojo(val masjidId: Int, val type: SalaahType, val datetime: GregorianCalendar) {
+    var id: Int? = null
+    var masjidName: String? = null
+
+    constructor(masjidId: Int, masjidName: String, type: SalaahType,
+                datetime: GregorianCalendar) : this(masjidId, type, datetime) {
+        this.masjidName = masjidName
+    }
+}
