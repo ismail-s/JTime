b1c9419bf1d293164231991e4dcb9d575a1b2b74
Ismail S <ismail-s.no-reply@github.com>
2016-09-24 17:50:58 +0100

    Use RobolectricTestRunner instead of RobolectricGradleTestRunner

--- JTime-android/src/test/java/com/ismail_s/jtime/android/MainActivityTest.kt
+++ JTime-android/src/test/java/com/ismail_s/jtime/android/MainActivityTest.kt
@@ -10,13 +10,13 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
-import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
-@RunWith(RobolectricGradleTestRunner::class)
+@RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class, sdk = intArrayOf(21))
 class MainActivityTest {
     lateinit var activity: MainActivity
