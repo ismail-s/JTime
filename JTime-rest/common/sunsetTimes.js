f44d816d3cede9d5a1d5a66f51daffb815787a9d
Ismail <astroman3d@gmail.com>
2016-10-25 15:51:25 +0000

    Add lru cache to getSunsetTime function

--- JTime-rest/common/sunsetTimes.js
+++ JTime-rest/common/sunsetTimes.js
@@ -1,28 +1,36 @@
 var Promise = require("bluebird");
 var request = require('request');
 var moment = require("moment");
+var cacheManager = require('cache-manager');
+var memoryCache = cacheManager.caching({store: 'memory', max: 10000, ttl: 2 * 24 * 3600/*seconds*/, promiseDependency: Promise});
 var settings = require('../settings');
 
 function getSunsetTime(location, date) {
-    var options = {
-        uri: 'http://api.geonames.org/timezoneJSON',
-        qs: {
+    //Date is normalised (ie info about hour/min/seconds is thrown away) for
+    //caching purposes
+    var normalisedDate = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0, 0));
+    var key = [location.lat.toString(), location.lng.toString(), normalisedDate].join(',');
+    return memoryCache.wrap(key, function() {
+       var options = {
+           uri: 'http://api.geonames.org/timezoneJSON',
+            qs: {
                 lat: location.lat,
                 lng: location.lng,
                 date: moment(date).format("YYYY-MM-DD"),
                 username: settings.geoNamesUsername
             },
             json: true
-    };
-    return new Promise(function(resolve, reject) {
-        request(options, function(error, response, body) {
-            if (error || response.statusCode != 200 || body.status || !body.sunset) {
-                console.warn("Error in geonames response", error, response, body);
-                reject(error || body.status);
-            } else {
-                resolve(new Date(body.sunset));
-            }
-        });
+       };
+       return new Promise(function(resolve, reject) {
+           request(options, function(error, response, body) {
+               if (error || response.statusCode != 200 || body.status || !body.sunset) {
+                   console.warn("Error in geonames response", error, response, body);
+                   reject(error || body.status);
+               } else {
+                   resolve(new Date(body.sunset));
+               }
+           });
+       });
     });
 }
 
