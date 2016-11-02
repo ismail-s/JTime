db28636cc32d1ea8c6bee24a07ac0a5ff12c3de0
Ismail <astroman3d@gmail.com>
2016-11-02 16:26:21 +0000

    Improve All Masjids card layout

--- JTime-rest/client/jtime-website/src/components/AllMasjids.vue
+++ JTime-rest/client/jtime-website/src/components/AllMasjids.vue
@@ -2,14 +2,12 @@
   <div>
     <h2>All Masjids</h2>
     <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--3-col-desktop mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--6-col-phone" v-for="masjid in masjids">
-        <router-link :to="linkToMasjid(masjid.id)">
-          <div class="mdl-card mdl-shadow--2dp">
+        <div class="mdl-cell mdl-card mdl-shadow--2dp mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--6-col-phone" v-for="masjid in masjids">
+          <router-link :to="linkToMasjid(masjid.id)">
             <div class="mdl-card__title">{{ masjid.name }}</div>
             <div class="mdl-card__supporting-text">{{ masjid.humanReadableAddress }}</div>
-          </div>
-        </router-link>
-      </div>
+          </router-link>
+        </div>
     </div>
   </div>
 </template>
