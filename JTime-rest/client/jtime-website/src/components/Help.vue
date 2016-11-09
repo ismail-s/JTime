0b8ee494cacfd089f7135a169a24dfbf1e1350d6
Ismail <astroman3d@gmail.com>
2016-11-09 11:27:19 +0000

    Improve text on Home & Help pages

--- JTime-rest/client/jtime-website/src/components/Help.vue
+++ JTime-rest/client/jtime-website/src/components/Help.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <p>This will be some help text...</p>
+    <h2>Help</h2>
+    <p>This website allows you to view masjid jamaat times, as well as add/change
+      the times. If anyone makes any changes, then everyone sees the new
+      version. It's a bit like editing wikipedia in that way.</p>
+    <p>There is an
+      <a href="https://play.google.com/store/apps/details?id=com.ismail_s.jtime.android">Android app</a>
+      available on the Play Store. It does the same job as this website, but is
+      a lot more developed.</p>
+    <p>This website is a work in progress, so a lot of features aren't available
+      yet. If you have any ideas/improvements/issues, please open an issue on
+      <a href="https://www.github.com/ismail-s/jtime/issues">Github</a>.</p>
+    <h3>Open source libraries used</h3>
+    <ul>
+      <li><a href="https://vuejs.org/">Vuejs</a> (MIT license)</li>
+      <li><a href="https://vuex.vuejs.org/">Vuex</a> (MIT license)</li>
+      <li><a href="https://router.vuejs.org/">Vue-router</a> (MIT license)</li>
+      <li><a href="https://github.com/pagekit/vue-resource">Vue-resource</a> (MIT license)</li>
+    </ul>
+    <p>Note that these are the libraries directly used in the final website.
+      There are a lot of libraries used across this whole project. More details
+      can be found by browsing the code on
+      <a href="https://www.github.com/ismail-s/jtime">Github</a>.</p>
   </div>
 </template>
 
--- JTime-rest/client/jtime-website/src/components/Home.vue
+++ JTime-rest/client/jtime-website/src/components/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h1>This is the home page</h1>
+    <h2>Jamaat Times</h2>
+    <p>Right now, this website is still in development (hence the sparse
+      homepage). Some features are available via the navigation drawer on the
+      left-hand side. <br/>
+      For more info on how the development is progressing, and to contribute,
+      please check out the project on
+      <a href="https://www.github.com/ismail-s/jtime">Github</a>.</p>
   </div>
 </template>
 
