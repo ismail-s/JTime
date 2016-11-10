03ffa33a35560bfd0c0f6431ec53ee36faba23d2
Ismail <astroman3d@gmail.com>
2016-11-10 15:48:03 +0000

    Sort out favicon filepaths & location in repo

--- JTime-rest/client/jtime-website/build/dev-server.js
+++ JTime-rest/client/jtime-website/build/dev-server.js
@@ -57,6 +57,7 @@ app.use(hotMiddleware)
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
+app.use(config.dev.assetsPublicPath, express.static('./staticRoot'))
 
 module.exports = app.listen(port, function (err) {
   if (err) {
