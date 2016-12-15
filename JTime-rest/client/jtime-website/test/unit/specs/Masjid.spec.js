11f380811c1edf19c8bf78a351a8e69b85a927fc
Ismail <astroman3d@gmail.com>
2016-12-15 15:10:13 +0000

    Update jtime-website tests

--- JTime-rest/client/jtime-website/test/unit/specs/Masjid.spec.js
+++ JTime-rest/client/jtime-website/test/unit/specs/Masjid.spec.js
@@ -1,40 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import moment from 'moment'
 import router from '../../../src/router'
 import Masjid from 'src/components/Masjid'
-
-// For some reason, in the test environment Array.find doesn't exist. This is
-// a polyfill for it.
-if (!Array.prototype.find) {
-  Object.defineProperty(Array.prototype, 'find', { // eslint-disable-line no-extend-native
-    value: function (predicate) {
-      'use strict'
-      if (this == null) {
-        throw new TypeError('Array.prototype.find called on null or undefined')
-      }
-      if (typeof predicate !== 'function') {
-        throw new TypeError('predicate must be a function')
-      }
-      var list = Object(this)
-      var length = list.length >>> 0
-      var thisArg = arguments[1]
-      var value
-
-      for (var i = 0; i < length; i++) {
-        value = list[i]
-        if (predicate.call(thisArg, value, i, list)) {
-          return value
-        }
-      }
-      return undefined
-    }
-  })
-}
+import 'babel-polyfill'
 
 describe('Masjid.vue', () => {
   function setUpComponent (masjids = []) {
-    router.push({name: 'masjid-id-date', params: {id: 1, date: '2016-01-01'}})
-    const mockStore = {actions: {getMasjids: sinon.spy(), getSalaahTimes: sinon.spy()}, state: {MasjidsModule: {masjids}, SalaahTimesModule: {salaahTimes: {}}}}
+    router.push({name: 'masjid-times-for-month', params: {id: 1, year: 2016, month: 0}})
+    const mockStore = {actions: {getMasjids: sinon.spy(), getSalaahTimesForMonth: sinon.spy()}, state: {MasjidsModule: {masjids}, SalaahTimesModule: {salaahTimes: {}}}}
     const vm = new Vue({
       el: document.createElement('div'),
       render: (h) => h(Masjid),
@@ -45,9 +19,22 @@ describe('Masjid.vue', () => {
   }
   it('should initially display "Loading...", along with the table titles', () => {
     const [vm] = setUpComponent()
-    expect(vm.$el.textContent).to.contain('Loading...')
-    expect(vm.$el.textContent).to.contain('Salaah type')
-    expect(vm.$el.textContent).to.contain('Time')
+    const strings = ['Loading...', 'Date', 'Day', 'Fajr', 'Zohar', 'Asr', 'Magrib', 'Esha']
+    strings.forEach(text => {
+      expect(vm.$el.textContent).to.contain(text)
+    })
+  })
+
+  it('displays each day of the month and day of the week', () => {
+    const [vm] = setUpComponent()
+    const daysInMonth = moment().year(2016).month(0).daysInMonth()
+    const strings = [...Array(daysInMonth).keys()].map(i => {
+      const day = moment().year(2016).month(0).date(i + 1).format('ddd')
+      return `${i + 1} ${day}`
+    })
+    strings.forEach(text => {
+      expect(vm.$el.textContent).to.contain(text)
+    })
   })
 
   it('should dispatch a getMasjids action when it can\'t find the masjid in the current state', () => {
@@ -64,27 +51,39 @@ describe('Masjid.vue', () => {
 
   it('dispatchs a getSalaahTimes action on component creation', () => {
     const [, mockStore] = setUpComponent()
-    expect(mockStore.actions.getSalaahTimes)
+    expect(mockStore.actions.getSalaahTimesForMonth)
       .to.have.been.calledWith(sinon.match.any, sinon.match((val) => {
-        return val.type === 'getSalaahTimes' &&
+        return val.type === 'getSalaahTimesForMonth' &&
           val.masjidId === 1 &&
-          val.date.getTime() === new Date('2016-01-01').getTime()
+          val.year === 2016 &&
+          val.month === 0
       }))
   })
 
-  it('displays salaahTimes when they are added to the store', (done) => {
+  it('displays salaahTimes when they are added to the store & are for the correct month', (done) => {
     const [vm, mockStore] = setUpComponent()
-    mockStore.state.SalaahTimesModule.salaahTimes = {'1': [{'type': 'f', 'datetime': '2016-11-07T06:40:17.354Z'}, {'type': 'z', 'datetime': '2016-11-07T13:00:58.374Z'}, {'type': 'a', 'datetime': '2016-11-07T15:00:58.374Z'}, {'type': 'e', 'datetime': '2016-11-07T18:30:58.374Z'}]}
+    mockStore.state.SalaahTimesModule.salaahTimes = {'1': [{'type': 'f', 'datetime': new Date('2016-01-01T06:40:17.354Z')}, {'type': 'z', 'datetime': new Date('2016-01-02T13:00:58.374Z')}, {'type': 'a', 'datetime': new Date('2016-01-03T15:00:58.374Z')}, {'type': 'm', 'datetime': new Date('2016-01-03T16:05:58.374Z')}, {'type': 'e', 'datetime': new Date('2016-01-04T18:30:58.374Z')}]}
     Vue.nextTick(() => {
       const text = vm.$el.textContent
-      expect(text).to.contain('Fajr')
-      expect(text).to.contain('Zohar')
-      expect(text).to.contain('Asr')
-      expect(text).to.contain('Esha')
       expect(text).to.contain('06-40')
       expect(text).to.contain('13-00')
       expect(text).to.contain('15-00')
+      expect(text).to.contain('16-05')
       expect(text).to.contain('18-30')
+      done()
+    })
+  })
+
+  it('doesn\'t display salaahTimes from other months', (done) => {
+    const [vm, mockStore] = setUpComponent()
+    mockStore.state.SalaahTimesModule.salaahTimes = {'1': [{'type': 'f', 'datetime': new Date('2016-02-01T06:40:17.354Z')}, {'type': 'z', 'datetime': new Date('2016-03-02T13:00:58.374Z')}, {'type': 'a', 'datetime': new Date('2017-01-03T15:00:58.374Z')}, {'type': 'm', 'datetime': new Date('2015-01-03T16:05:58.374Z')}, {'type': 'e', 'datetime': new Date('2016-12-04T18:30:58.374Z')}]}
+    Vue.nextTick(() => {
+      const text = vm.$el.textContent
+      expect(text).to.not.contain('06-40')
+      expect(text).to.not.contain('13-00')
+      expect(text).to.not.contain('15-00')
+      expect(text).to.not.contain('16-05')
+      expect(text).to.not.contain('18-30')
       done()
     })
   })
