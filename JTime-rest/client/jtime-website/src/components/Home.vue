e5dbc29f8c589831c6acb3385d4b9687a7b71a11
Ismail <astroman3d@gmail.com>
2017-01-18 17:18:58 +0000

    Get location, display nearby times in home screen

--- JTime-rest/client/jtime-website/src/components/Home.vue
+++ JTime-rest/client/jtime-website/src/components/Home.vue
@@ -1,23 +1,86 @@
 <template>
   <div>
     <h2>Jamaat Times</h2>
-    <p>Right now, this website is still in development (hence the sparse
-      homepage). Some features are available via the navigation drawer on the
-      left-hand side. <br/>
-      For more info on how the development is progressing, and to contribute,
-      please check out the project on
-      <a href="https://www.github.com/ismail-s/jtime">Github</a>.</p>
+    <p v-if="!locationApiIsAvailable">Finding your current location is not
+                                      supported by your browser</p>
+    <p v-else-if="locationOrError === null">Trying to get your location...</p>
+    <p v-else-if="locationOrErrorIsError">Weren't able to get your location</p>
+    <div class="mdl-grid" v-else>
+        <div class="mdl-cell mdl-card mdl-shadow--2dp mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--6-col-phone" v-for="salaahTimes in sortedSalaahTimes">
+          <div class="mdl-card__title">{{ salaahTypeCodeToString(salaahTimes[0]) }}</div>
+          <div class="mdl-card__supporting-text">
+            <table class="mdl-data-table mdl-js-data-table center">
+              <tbody>
+                <tr v-for="time in salaahTimes[1]">
+                  <td class="mdl-data-table__cell--non-numeric">{{ time.masjidName }}</td>
+                  <td>{{ time.datetime | format_as_hh_mm }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+import {salaahTypeCodeToString} from '../utils'
+
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      locationApiIsAvailable: true,
+      locationOrError: null
+    }
+  },
+  methods: {
+    salaahTypeCodeToString
+  },
+  filters: {
+    format_as_hh_mm: d => moment(d).format('HH-mm')
+  },
+  computed: {
+    locationOrErrorIsError () {
+      return !this.locationOrError ||
+        this.locationOrError.code ||
+        !this.locationOrError.coords ||
+        !this.locationOrError.coords.latitude ||
+        !this.locationOrError.coords.longitude
+    },
+    sortedSalaahTimes () {
+      return this.$store.state.SalaahTimesModule.nearbySalaahTimes
+    }
+  },
+  mounted () {
+    if ('geolocation' in window.navigator) {
+      window.navigator.geolocation.getCurrentPosition((position) => {
+        this.locationOrError = position
+        this.$store.dispatch({
+          type: 'getTimesForNearbyMasjids',
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        })
+      }, (error) => {
+        this.locationOrError = error
+      }, {
+        timeout: 15000,
+        maximumAge: 15000
+      })
+    } else {
+      this.locationApiIsAvailable = false
+    }
+  }
 }
 </script>
 
 <style scoped>
 h1 {
   font-weight: normal;
+}
+
+.center {
+  margin: auto;
 }
 </style>
--- JTime-rest/client/jtime-website/src/store/salaahtimes.js
+++ JTime-rest/client/jtime-website/src/store/salaahtimes.js
@@ -1,3 +1,5 @@
+import haversine from 'haversine'
+import moment from 'moment'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import {dateToDateString} from '../utils'
@@ -7,11 +9,15 @@ Vue.use(VueResource)
 
 export default {
   state: {
-    salaahTimes: {} // map from masjidId to list of salaahTime objects
+    salaahTimes: {}, // map from masjidId to list of salaahTime objects
+    nearbySalaahTimes: [] // list of lists of the form [salaahType, salaah_times]
   },
   mutations: {
     addSalaahTimes (state, {masjidId, times}) {
       Vue.set(state.salaahTimes, masjidId, times)
+    },
+    addNearbySalaahTimes (state, newTimes) {
+      state.nearbySalaahTimes = newTimes
     }
   },
   actions: {
@@ -31,6 +37,55 @@ export default {
         // Make sure times returned by rest api are for the correct month/year
         }).filter(time => time.datetime.getFullYear() === year && time.datetime.getMonth() === month)
         context.commit('addSalaahTimes', {masjidId, times: newTimes})
+      })
+    },
+    getTimesForNearbyMasjids (context, {latitude, longitude}) {
+      const date = new Date()
+      const options = {params: {date: dateToDateString(date)}, location: {lat: latitude, lng: longitude}}
+      Vue.http.get(`${baseUrl}/SalaahTimes/times-for-multiple-masjids`, options).then(response => {
+        return response.json()
+      }).then(({res}) => {
+        const newTimes = res.map(time => {
+          if (typeof time.datetime === 'string') {
+            time.datetime = new Date(time.datetime)
+          }
+          return time
+        // Make sure times returned by rest api are for today
+        }).filter(time => moment().format('YYYY-DDD') === moment(time).format('YYYY-DDD'))
+        .reduce((acc, elem) => {
+          if (acc[elem.type]) {
+            acc[elem.type].push(elem)
+            return acc
+          }
+        }, {'f': [], 'z': [], 'a': [], 'm': [], 'e': []})
+        var newTimesAsList = [['f', newTimes['f']], ['z', newTimes['z']],
+          ['a', newTimes['a']], ['m', newTimes['m']], ['e', newTimes['e']]]
+          .map(([salaahType, times]) => {
+            const sortedTimes = times.sort((a, b) => {
+              // sort by time, then by distance from current possition
+              const [aTime, bTime] = [a.getTime(), b.getTime()]
+              if (aTime < bTime) {
+                return -1
+              } else if (aTime > bTime) {
+                return 1
+              } else {
+                const currentLoc = {latitude, longitude}
+                const aLoc = {latitude: a.location.lat, longitude: a.location.lng}
+                const bLoc = {latitude: b.location.lat, longitude: b.location.lng}
+                const aDist = haversine(currentLoc, aLoc)
+                const bDist = haversine(currentLoc, bLoc)
+                if (aDist < bDist) {
+                  return -1
+                } else if (aDist > bDist) {
+                  return 1
+                } else {
+                  return 0
+                }
+              }
+            })
+            return [salaahType, sortedTimes]
+          })
+        context.commit('addNearbySalaahTimes', newTimesAsList)
       })
     }
   }
