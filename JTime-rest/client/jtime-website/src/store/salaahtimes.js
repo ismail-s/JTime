70e3f0de78cec4858871cb9bf459fdda5d23fe8e
Ismail <astroman3d@gmail.com>
2017-01-19 17:20:40 +0000

    Move code around, add updateTime, closestTime computed properties

--- JTime-rest/client/jtime-website/src/store/salaahtimes.js
+++ JTime-rest/client/jtime-website/src/store/salaahtimes.js
@@ -1,4 +1,3 @@
-import haversine from 'haversine'
 import moment from 'moment'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
@@ -10,7 +9,7 @@ Vue.use(VueResource)
 export default {
   state: {
     salaahTimes: {}, // map from masjidId to list of salaahTime objects
-    nearbySalaahTimes: [] // list of lists of the form [salaahType, salaah_times]
+    nearbySalaahTimes: [] // list of salaahTime objects
   },
   mutations: {
     addSalaahTimes (state, {masjidId, times}) {
@@ -41,51 +40,18 @@ export default {
     },
     getTimesForNearbyMasjids (context, {latitude, longitude}) {
       const date = new Date()
-      const options = {params: {date: dateToDateString(date)}, location: {lat: latitude, lng: longitude}}
+      const options = {params: {date: dateToDateString(date), location: {lat: latitude, lng: longitude}}}
       Vue.http.get(`${baseUrl}/SalaahTimes/times-for-multiple-masjids`, options).then(response => {
         return response.json()
       }).then(({res}) => {
         const newTimes = res.map(time => {
           if (typeof time.datetime === 'string') {
-            time.datetime = new Date(time.datetime)
+            time.datetime = moment(time.datetime).seconds(0).milliseconds(0).toDate()
           }
           return time
         // Make sure times returned by rest api are for today
         }).filter(time => moment().format('YYYY-DDD') === moment(time).format('YYYY-DDD'))
-        .reduce((acc, elem) => {
-          if (acc[elem.type]) {
-            acc[elem.type].push(elem)
-            return acc
-          }
-        }, {'f': [], 'z': [], 'a': [], 'm': [], 'e': []})
-        var newTimesAsList = [['f', newTimes['f']], ['z', newTimes['z']],
-          ['a', newTimes['a']], ['m', newTimes['m']], ['e', newTimes['e']]]
-          .map(([salaahType, times]) => {
-            const sortedTimes = times.sort((a, b) => {
-              // sort by time, then by distance from current possition
-              const [aTime, bTime] = [a.getTime(), b.getTime()]
-              if (aTime < bTime) {
-                return -1
-              } else if (aTime > bTime) {
-                return 1
-              } else {
-                const currentLoc = {latitude, longitude}
-                const aLoc = {latitude: a.location.lat, longitude: a.location.lng}
-                const bLoc = {latitude: b.location.lat, longitude: b.location.lng}
-                const aDist = haversine(currentLoc, aLoc)
-                const bDist = haversine(currentLoc, bLoc)
-                if (aDist < bDist) {
-                  return -1
-                } else if (aDist > bDist) {
-                  return 1
-                } else {
-                  return 0
-                }
-              }
-            })
-            return [salaahType, sortedTimes]
-          })
-        context.commit('addNearbySalaahTimes', newTimesAsList)
+        context.commit('addNearbySalaahTimes', newTimes)
       })
     }
   }
