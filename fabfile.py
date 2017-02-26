5cba8936aaeab2f63f93e613be4c56c0fd133366
Ismail S <ismail-s.no-reply@github.com>
2017-02-26 18:09:32 +0000

    Upgrade Gradle Play Publisher plugin, fix rsync command

--- fabfile.py
+++ fabfile.py
@@ -7,5 +7,5 @@ def deploy():
         run('git stash && git pull && git stash pop')
         run('npm install .')
         rsync_project(remote_dir = '~/JTime/JTime-rest/client/jtime-website',
-                      local_dir = 'JTime-rest/client/jtime-website/build')
+                      local_dir = 'JTime-rest/client/jtime-website/dist')
         run('pm2 reload .')
