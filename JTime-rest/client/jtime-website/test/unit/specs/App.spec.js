86e0f4f85a2f1f848c0c55c249b460fc49068b1b
Ismail <astroman3d@gmail.com>
2017-03-13 15:59:49 +0000

    Add tests

---
+++ JTime-rest/client/jtime-website/test/unit/specs/App.spec.js
@@ -0,0 +1,64 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import App from 'src/App'
+
+Vue.use(VueRouter)
+
+const testComponent = { template: '<div>test</div>' }
+
+// Note that the paths in these routes aren't so important for testing purposes,
+// as long as they are distinct from each other.
+const routes = [
+  { path: '/', name: 'home' },
+  { path: '/all-masjids', name: 'all-masjids' },
+  { path: '/help', name: 'help' },
+  { path: '/masjid/id/year/month', name: 'masjid-times-for-month' },
+  { path: '/masjid/id/year/month/edit', name: 'edit-salaah-times' }
+].map((e) => {
+  e.component = testComponent
+  return e
+})
+
+const mockStore = {getters: {loggedIn: () => false}}
+
+describe('App.vue', () => {
+  let vm, router
+  beforeEach(() => {
+    router = new VueRouter({routes})
+    vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(App),
+      router,
+      store: new Vuex.Store(mockStore)
+    })
+  })
+
+  it('should display Home, All Masjids and Help navbar', () => {
+    const text = vm.$el.textContent
+    expect(text).to.match(/Home\s+All Masjids\s+Help/)
+  })
+
+  it('should display Home as the title', () => {
+    const elem = vm.$el.getElementsByClassName('mdl-layout-title')[0]
+    expect(elem.textContent).to.match(/^Home/)
+  })
+
+  const changingRouteTests = [
+    { routeName: 'all-masjids', expected: 'All masjids' },
+    { routeName: 'help', expected: 'Help' },
+    { routeName: 'masjid-times-for-month', expected: 'Masjid times' },
+    { routeName: 'edit-salaah-times', expected: 'Change salaah times' }
+  ]
+
+  changingRouteTests.forEach(e => {
+    it(`should display ${e.expected} as the title when navigating to that route`, (done) => {
+      router.push({name: e.routeName})
+      Vue.nextTick(() => {
+        const elem = vm.$el.getElementsByClassName('mdl-layout-title')[0]
+        expect(elem.textContent).to.match(new RegExp(`^${e.expected}`))
+        done()
+      })
+    })
+  })
+})
